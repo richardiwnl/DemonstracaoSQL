a8735c18bb097125872650dec7a3bcc1b10c25ca
Richard <richardiwnl@outlook.com>
2022-01-10 02:33:41 -0300

    Commit inicial

---
+++ src/main/java/css/richard/demonstracaosql/DemonstracaosqlApplication.java
@@ -0,0 +1,15 @@
+package css.richard.demonstracaosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstracaosqlApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemonstracaosqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/css/richard/demonstracaosql/DemonstracaosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package css.richard.demonstracaosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstracaosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
