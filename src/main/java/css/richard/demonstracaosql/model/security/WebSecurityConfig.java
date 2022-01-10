7610de7aafe45c5317e6104b358408d149a05c84
Richard <richardiwnl@outlook.com>
2022-01-10 03:01:45 -0300

    Configurações de segurança inicias definidas

---
+++ src/main/java/css/richard/demonstracaosql/model/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package css.richard.demonstracaosql.model.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.io.IOException;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Override
+    public void configure(WebSecurity security)
+    {
+
+        security
+                .ignoring()
+                .antMatchers("/css/**", "/js/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception
+    {
+
+        security
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/registrar").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/usuarios/1")
+                .usernameParameter("email")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/");
+    }
+}
