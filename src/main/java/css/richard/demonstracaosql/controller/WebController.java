77d908eb0ee114981b1567499cab1d0d418268c8
Richard <richardiwnl@outlook.com>
2022-01-10 03:26:48 -0300

    Redirecionamento da URL raíz para a página de login adicionado

--- src/main/java/css/richard/demonstracaosql/controller/WebController.java
+++ src/main/java/css/richard/demonstracaosql/controller/WebController.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/")
 public class WebController
 {
 
     @GetMapping
+    public void root(HttpServletResponse response) throws IOException
+    {
+
+        response.sendRedirect("/login");
+    }
+
+
+    @GetMapping("/login")
     public String home(Model model)
     {
 
