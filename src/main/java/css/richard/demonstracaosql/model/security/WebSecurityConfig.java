85d10edfa94db4922dab8d29e86578d74f28865b
Richard <richardiwnl@outlook.com>
2022-01-10 03:55:41 -0300

    Adicionado Bean de DaoAuthenticationProvider para realizar a autenticação

--- src/main/java/css/richard/demonstracaosql/model/security/WebSecurityConfig.java
+++ src/main/java/css/richard/demonstracaosql/model/security/WebSecurityConfig.java
@@ -1,15 +1,43 @@
 package css.richard.demonstracaosql.model.security;
 
+import css.richard.demonstracaosql.model.services.CustomUserDetailsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        return new CustomUserDetailsServices();
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider authenticationProvider =
+                new DaoAuthenticationProvider();
+
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        authenticationProvider.setUserDetailsService(userDetailsService());
+
+        return authenticationProvider;
+    }
 
     @Override
     public void configure(WebSecurity security)
