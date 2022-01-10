12edf72e2d7f333deb678eb66289c0ed990144af
Richard <richardiwnl@outlook.com>
2022-01-10 04:51:31 -0300

    URL de logout sucedido alterada

--- src/main/java/css/richard/demonstracaosql/model/security/WebSecurityConfig.java
+++ src/main/java/css/richard/demonstracaosql/model/security/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                 .and()
                 .logout()
                 .logoutUrl("/logout").permitAll()
-                .logoutSuccessUrl("/login").permitAll()
+                .logoutSuccessUrl("/").permitAll()
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID");
